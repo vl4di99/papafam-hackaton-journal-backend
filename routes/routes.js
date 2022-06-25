f5fdf58946d9415910a23e49240d163a51154faa
Dionisie-Vladut Lorincz <dionisie.lorincz@yahoo.com>
2022-06-25 23:37:46 +0300

    Simplified backend

--- routes/routes.js
+++ routes/routes.js
@@ -5,23 +5,4 @@ const express = require("express");
 // The router will be added as a middleware and will take control of requests starting with path /listings.
 const createdRoutes = express.Router();
 
-// This will help us connect to the database
-const dbo = require("../db/conn");
-
-createdRoutes.route("/journalRecords").get(async function (_req, res) {
-  const dbConnect = dbo.getDb();
-
-  dbConnect
-    .collection("journals")
-    .find({})
-    .limit(50)
-    .toArray(function (err, result) {
-      if (err) {
-        res.status(400).send("Error fetching records!");
-      } else {
-        res.json(result);
-      }
-    });
-});
-
 module.exports = createdRoutes;
--- server.js
+++ server.js
@@ -3,30 +3,47 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 // get MongoDB driver connection
-const dbo = require("./db/conn");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/routes"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Global error handling
-app.use(function (err, _req, res) {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+app.use(function (err, req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
 // perform a database connection when the server starts
-dbo.connectToServer(function (err) {
-  if (err) {
-    console.error(err);
-    process.exit();
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err) {
+    if (err) throw err;
   }
+);
 
-  // start the Express server
-  app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-  });
+const mongoData = mongoose.connection;
+mongoData.on("error", console.error.bind(console, "connection error: "));
+mongoData.once("open", function () {
+  console.log("Connected successfully to MongoDB");
+});
+
+app.use(require("./routes/routes"));
+
+app.get("/", (request, response, next) => {
+  response.json({ message: "Hey! This is your server response!" });
+  next();
 });
